9754d77bb4aa788aab711500219c16d4cfc4f5a5
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-02-18 04:31:57 +0530

    file uplode

---
+++ demo.py
@@ -0,0 +1,18 @@
+from us_visa.logger import logging
+
+logging.info("welcome to us_visa")
+
+logging.info("welcome to us_visa susant experiment")
+
+logging.info("welcome to us_visa experiment")
+
+
+logging.info("welcome to us_visa experiment susant")
+
+
+
+
+
+
+
+
