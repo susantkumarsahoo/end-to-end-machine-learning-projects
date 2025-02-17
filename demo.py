c28a46ce94df44b8eeef76cb431a1ce928f8058d
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-02-18 05:17:49 +0530

    file uplode

--- demo.py
+++ demo.py
@@ -1,15 +1,22 @@
 from us_visa.logger import logging
-
-logging.info("welcome to us_visa")
-
-logging.info("welcome to us_visa susant experiment")
-
-logging.info("welcome to us_visa experiment")
+from us_visa.exception import USvisaException
+import sys
 
 
-logging.info("welcome to us_visa experiment susant")
+
+# logging.info("welcome to us_visa")
+
+# logging.info("welcome to us_visa susant experiment")
+
+# logging.info("welcome to us_visa experiment")
 
 
+# logging.info("welcome to us_visa experiment susant")
+
+try:
+    a = 2/0
+except Exception as e:
+    logging.error(USvisaException(str(e), sys))
 
 
 
